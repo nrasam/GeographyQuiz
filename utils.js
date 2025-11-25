5a3513d3795b87183c3bd7313fd9c7b1a7a0db03
amicit05 <amicit05@my.yorku.ca>
2025-11-25 15:46:04 -0500

    Country and Cities button

--- utils.js
+++ utils.js
@@ -47,3 +47,74 @@ function optionsContinent(num){ //Parameter is the correct answer which later ha
   return shuffledAnswer; //Returns the shuffled answers containing the correct and wrong answers!
 }
 //MIXING POSSIBLE ANSWERS END
+
+//MIXING POSSIBLE ANSWERS START - COUNTRIES
+function optionCountries(num){
+  let countries = ["Brazil","Peru","France","Australia","China","India","Egypt","Turkey","UK","USA","Italy","Iran","Spain"]; //Array with all the countries
+  let chosenCountries = []; //Array that will contain the correct answer, plus the 3 wrong answers
+  let numberChosen = new Set(); //This set will avoid that the same country appears on the round.
+  numberChosen.add(countries[num]); //Saves the first CORRECT country inside the set.
+  chosenCountries.push(countries[num]); //Saves the first CORRECT country into the answers array.
+  for(let i=0; chosenCountries.length<4; i++){ //Repeat this steps until the 4 spaces have been filled.
+    let number = 0; //Let there be a variable to choose a random index.
+    number = Math.floor(Math.random()*countries.length); //Let the variable number be an index from 0-13.
+    if(numberChosen.has(countries[number])){ //If the country is already found inside the set then:
+      while(numberChosen.has(countries[number])){ //Continue looking for a country until finding one that is NOT found inside the set.
+        number = Math.floor(Math.random()*countries.length); //Continue calculating a random number between 0 and 13.
+      }
+    }
+    numberChosen.add(countries[number]); //Add the new country inside the set.
+    chosenCountries.push(countries[number]); //Add the new country inside the array.
+  }
+  //Random order -->
+  let countriesNewOrder = []; //This array will contain the mixed order answers.
+  let newSet = new Set(); //This set will make sure that the same answer is not repeated more then once.
+  for(let i=0; countriesNewOrder.length < chosenCountries.length; i++){ //Repeats this process until the length of the mixed array reaches the same length of the original answer array.
+    let number = Math.floor(Math.random()*chosenCountries.length); //Let the variable number be an index from 0-3.
+    if(newSet.has(number)){ //If the set already has this number:
+      while(newSet.has(number)){ //Continue looking for a number that is NOT found inside the set.
+        number = Math.floor(Math.random()*chosenCountries.length); //Continue calculating a random number between 0 and 3.
+      }
+    }
+    newSet.add(number); //Add the new number inside the set.
+    countriesNewOrder.push(chosenCountries[number]); //Add the selected country inside the new mixed array.
+  }
+
+  return countriesNewOrder; //Returns the mixed array.
+}
+//MIXING POSSIBLE ANSWERS END - COUNTRIES
+
+//MIXING POSSIBLE ANSWERS START - CITIES
+function optionCities(num){
+let cities = ["Rio de Janeiro","Cusco","Paris","Sydney","Beijing","Agra","Cairo","Capadocia","London","New York","Rome","Salisbury","Tehran","Valencia"]; //Array of cities
+let chosenCities = []; //Array that will contain the correct answer, plus the 3 wrong answers
+let numberChosen = new Set(); //This set will avoid that the same city appears on the round.
+numberChosen.add(cities[num]); //Stores the city inside the set.
+chosenCities.push(cities[num]); //Stores the city inside the answers array.
+for(let i=0; chosenCities.length<4; i++){ //Repeat this steps until the 4 spaces have been filled.
+    let number = 0; //Let there be a variable to choose a random index.
+    number = Math.floor(Math.random()*cities.length); //Let the variable number be an index from 0-14.
+    if(numberChosen.has(cities[number])){ //If the city is already found inside the set then:
+      while(numberChosen.has(cities[number])){ //Continue looking for a city until finding one that is NOT found inside the set.
+        number = Math.floor(Math.random()*cities.length); //Continue calculating a random number between 0 and 14.
+      }
+    }
+    numberChosen.add(cities[number]); //Add the new city inside the set.
+    chosenCities.push(cities[number]); //Add the new city inside the array.
+  }
+  //Random order -->
+  let citiesNewOrder = []; //This array will contain the mixed order answers.
+  let newSet = new Set(); //This set will make sure that the same answer is not repeated more then once.
+  for(let i=0; citiesNewOrder.length < chosenCities.length; i++){ //Repeats this process until the length of the mixed array reaches the same length of the original answer array.
+    let number = Math.floor(Math.random()*chosenCities.length); //Let the variable number be an index from 0-3.
+    if(newSet.has(number)){ //If the set already has this number:
+      while(newSet.has(number)){ //Continue looking for a number that is NOT found inside the set.
+        number = Math.floor(Math.random()*chosenCities.length); //Continue calculating a random number between 0 and 3.
+      }
+    }
+    newSet.add(number); //Add the new number inside the set.
+    citiesNewOrder.push(chosenCities[number]); //Add the selected city inside the new mixed array.
+  }
+  return citiesNewOrder;
+}
+//MIXING POSSIBLE ANSWERS END - CITIES
